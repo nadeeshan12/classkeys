d06a49a74529022f315dcc6100ed912b921f7591
nadeeshan12 <umashiduwara890@gmail.com>
2025-06-01 15:29:06 +0530

    Add files via upload

---
+++ papers/script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.paper-button').forEach(button => {
+    button.addEventListener('mousemove', (e) => {
+      const rect = button.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      button.style.setProperty('--x', `${x}px`);
+      button.style.setProperty('--y', `${y}px`);
+    });
+  });
+  
